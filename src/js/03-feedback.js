3cb56c22baa68ed173496b1c387bdd03af7e7da0
Betelgeuse22 <Don.vitaki@gmail.com>
2022-10-02 18:06:12 +0200

    Update task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_KEY = 'feedback-from-state';
+
+const formEl = document.querySelector('.feedback-form');
+const inputEmailEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
+
+/////////////////save data to localStarage///////////////////
+formEl.addEventListener('input', onInput);
+
+function onInput(evt) {
+  const userData = { email: inputEmailEl.value, message: textareaEl.value };
+
+  const userDataJSON = JSON.stringify(userData);
+  localStorage.setItem(LOCALSTORAGE_KEY, userDataJSON);
+}
+
+///////////////submit data//////////////////////////////
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+/////////////////auto-complete/////////////////////////////
+function populateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (savedMessage) {
+    inputEmailEl.value = savedMessage.email;
+    textareaEl.value = savedMessage.message;
+  }
+}
+
+populateTextarea();
