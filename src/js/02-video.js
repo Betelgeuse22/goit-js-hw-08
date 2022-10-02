4848e1e323657f06a563db6efd5b3078cfbd3558
Betelgeuse22 <Don.vitaki@gmail.com>
2022-10-02 13:05:56 +0200

    Update task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on('timeupdate', throttle(saveData, 1000));
+
+function saveData(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('play', data => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (data.seconds !== savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
