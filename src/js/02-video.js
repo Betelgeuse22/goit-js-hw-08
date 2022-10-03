d7e57d3bb24f52151674399fd4d40ce4466c934c
Betelgeuse22 <Don.vitaki@gmail.com>
2022-10-03 14:13:04 +0200

    Update 02.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,29 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const throttle = require('lodash.throttle');
+const CURRENT_TIME = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(saveData, 1000));
 
 function saveData(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(CURRENT_TIME, data.seconds);
 }
 
-player.on('play', data => {
-  const savedTime = localStorage.getItem('videoplayer-current-time');
-  if (data.seconds !== savedTime) {
-    player.setCurrentTime(savedTime);
-  }
-});
+player
+  .setCurrentTime(localStorage.getItem(CURRENT_TIME))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
